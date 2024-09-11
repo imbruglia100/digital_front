07f53b16516f8e07ac24d1c032347adc0811210d
frank <imbruglia100@gmail.com>
2024-09-11 19:18:07 -0400

    updates

--- react-vite/src/components/Home/Home.jsx
+++ react-vite/src/components/Home/Home.jsx
@@ -1,12 +1,108 @@
+/** @format */
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getStores } from "../../redux/stores";
+import { getProducts } from "../../redux/products";
+import StoreCard from "../StoreCard/StoreCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import "./Home.css";
+import ProductCard from "../ProductCard/ProductCard";
 
 const Home = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  const stores = useSelector((state) => state.stores.allStores);
+  const products = useSelector((state) => state.products.allProducts);
 
-export default Home
+  const storesArr =
+    Object.values(stores).length > 10
+      ? Object.values(stores).slice(0, 11)
+      : Object.values(stores);
+
+  const productsArr =
+    Object.values(products).length > 10
+      ? Object.values(products).slice(0, 11)
+      : Object.values(products);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getStores());
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <div className='home-container'>
+      <h1>Featured Stores</h1>
+      <p className='tagline'>
+        Discover the best local and online stores, carefully curated for your
+        convenience. Shop from trusted sellers and explore new favorites today.
+      </p>
+      {storesArr.length !== 0 ? (
+        <Carousel
+          swipeable={false}
+          draggable={false}
+          responsive={responsive}
+          infinite={true}
+          centerMode={true}
+          containerClass='carousel-container'
+          dotListClass='custom-dot-list-style'
+          itemClass='carousel-item'
+        >
+          {storesArr.map((store) => (
+            <StoreCard store={store} />
+          ))}
+        </Carousel>
+      ) : (
+        <p>Currently no stores</p>
+      )}
+
+      <h1>Featured Products</h1>
+      <p className='tagline'>
+        Browse our top-rated products, handpicked just for you. Find everything
+        you need, from everyday essentials to unique finds.
+      </p>
+
+      {productsArr.length !== 0 ? (
+        <Carousel
+          swipeable={false}
+          draggable={false}
+          responsive={responsive}
+          infinite={true}
+          centerMode={true}
+          containerClass='carousel-container'
+          dotListClass='custom-dot-list-style'
+          itemClass='carousel-item'
+        >
+          {productsArr.map((prod) => (
+            <ProductCard product={prod} />
+          ))}
+        </Carousel>
+      ) : (
+        <p>Currently no products</p>
+      )}
+    </div>
+  );
+};
+
+export default Home;
--- react-vite/src/components/ProductCard/ProductCard.jsx
+++ react-vite/src/components/ProductCard/ProductCard.jsx
@@ -4,16 +4,9 @@ import { NavLink } from "react-router-dom";
 import "./ProductCard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileLines, faStar } from "@fortawesome/free-solid-svg-icons";
-const ProductCard = ({ product, userProduct }) => {
+const ProductCard = ({ product }) => {
   return product ? (
-    <NavLink
-      to={
-        userProduct
-          ? `/products/${product?.id}`
-          : `${product?.id}`
-      }
-      className='product-card-container'
-    >
+    <NavLink to={`/products/${product?.id}`} className='product-card-container'>
       <div className='product-img-container'>
         <div className='banner-container'>
           <img className='banner' src={product?.product_img} />
@@ -29,15 +22,16 @@ const ProductCard = ({ product, userProduct }) => {
 
       <div className='product-info'>
         <div className='second-half-product-info'>
-        <p style={{fontSize: '20px', fontWeight:'600', margin:'5px'}}>${product.price}</p>
-          {
-            product?.description ? (
-              <p style={{ margin:'5px', textAlign:"center"}}>{product.description}</p>
-            )
-              :
-              <></>
-
-          }
+          <p style={{ fontSize: "20px", fontWeight: "600", margin: "5px" }}>
+            ${product.price}
+          </p>
+          {product?.description ? (
+            <p style={{ margin: "5px", textAlign: "center" }}>
+              {product.description}
+            </p>
+          ) : (
+            <></>
+          )}
           {product?.stock_amount <= 25 ? (
             <p className='product-amount'>
               {product?.stock_amount} left in stock!
@@ -46,19 +40,18 @@ const ProductCard = ({ product, userProduct }) => {
             <div></div>
           )}
         </div>
-          <div className='first-half-product-info'>
-
-            <a style={{color:"#2845c7"}}>
-              35 <FontAwesomeIcon icon={faFileLines} />
-            </a>
-            •
-            <a
-              style={{ color: "#A57C00", textDecoration: "none" }}
-              href='#reviews'
-            >
-              5.0 <FontAwesomeIcon icon={faStar} />
-            </a>
-          </div>
+        <div className='first-half-product-info'>
+          <a style={{ color: "#2845c7" }}>
+            35 <FontAwesomeIcon icon={faFileLines} />
+          </a>
+          •
+          <a
+            style={{ color: "#A57C00", textDecoration: "none" }}
+            href='#reviews'
+          >
+            5.0 <FontAwesomeIcon icon={faStar} />
+          </a>
+        </div>
       </div>
     </NavLink>
   ) : (
